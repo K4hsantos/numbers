538ce2e5f63f620eec479249335ee1b8d870f0b1
K4hsantos <kasantoshenrique@gmail.com>
2026-01-16 22:45:51 -0300

    Validate input and block submit flow when invalid

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const form = document.querySelector("form");
-const formLayout = document.querySelector(".form-layout")
+const formLayout = document.querySelector(".form-layout");
+const numberAmount = document.querySelector("#amount");
+const numberMin = document.querySelector("#min");
+const numberMax = document.querySelector("#max");
 
 
 form.addEventListener("submit", (event)=> {
 event.preventDefault();
+const numero = Number(numberAmount.value);
+const minimo = Number(numberMin.value);
+const maximo = Number(numberMax.value);
 
-formLayout.classList.add("show-result")
+const validate = validateNumbers(numero, minimo, maximo);
+
+if (validate === false) {
+    return false
+} else {
+    formLayout.classList.add("show-result")
+}
 })
+
+function validateNumbers(amount, minimum, maximum) {
+    if (amount <= 0) {
+        alert("n foi")
+        return false
+    } 
+    if (minimum >= maximum) {
+        alert("n foi")
+        return false
+    } else {
+        return true
+    }
+};
